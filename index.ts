6c96d07e5b4b01361a843ab5a97d957124a0c1c2
Sorin Vestemean <39151503+vestemeansorin91@users.noreply.github.com>
2022-06-29 14:32:28 +0300

    Update index.ts
    muta si port-ul in .env

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config(); 
+
 import express, { Application} from 'express';
 import cors from 'cors';
 import database from './src/config/database';
@@ -7,8 +10,6 @@ import uploadRouter from "./src/routes/uploads";
 import diaryRouter from "./src/routes/diaryRouter";
 
 import privateEndpoint from './src/middleware/privateEndpoint';
-import dotenv from "dotenv";
-dotenv.config();
 const app:Application = express();
 
 app.use(express.json());
@@ -17,43 +18,20 @@ app.use(express.urlencoded({
 }));
 
 app.use(cors());
-if(!process.env.DB_HOST){
-  throw "Please specify the DB host in .env file";
-}
-if(!process.env.DB_USERNAME){
-  throw "Please specify the DB username in .env file";
-}
-if(!process.env.DB_PASSWORD){
-  throw "Please specify the DB password in .env file";
-}
-if(!process.env.SECRET){
-  throw "Please specify the JWT secret in .env file";
-}
-if(!process.env.REFRESH_SECRET){
-  throw "Please specify the JWT refresh token in .env file";
-}
-if(!process.env.DATABASE_NAME){
-  throw "Please specify the DB name in .env file";
-}
+checkEnvConfigAvailable(); // trebuie sa creezi tu fisierul
+
 database.authenticate()
   .then(() => console.log("connected to postgres"))
   .catch((err) => console.log(err));
 
 app.use('/api/static', express.static(__dirname +'/static'));
 app.use(addUser);
+
 app.use("/api/auth", authRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/diary", diaryRouter);
 app.post("/api/test", privateEndpoint, (req, res) => {
   res.send({message: "esti logat inca"});
 });
-//Creating port variable from env variable or setting it mannualy
 
-const PORT = process.env.PORT || 3026;
-
-
-//Listening on the PORT variable and then console logging the port
-
-app.listen(PORT, () =>{
-    console.log(`Listening on port ${PORT}`);
-});
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
